7e95e5e9050b7c4a2c367e229a0db6e239b66afc
Nakul Bajaj <bajaj.nakul@gmail.com>
2026-02-08 16:07:50 +1100

    Updated readme

---
+++ config.py
@@ -0,0 +1,109 @@
+JOB_TITLE="Senior Engineering Manager - Machine Learning"
+
+COMPANY_NAME="REA GROUP"
+COMPANY_LOCATION="Melbourne Australia"
+
+JOB_DESCRIPTION="""This role is within the Membership and Leads to Keys Portfolio which sits within the Consumer Group of the business. Consumer Group’s mandate is to be the number one destination for Australian property and real estate through having the most engaging and best real estate and property experience. This includes the AI Workflows & Experience team (part of the Consumer Experience division), where you’ll help shape the future of the user property journey by building intelligent AI Assistants that enhance our web and app experiences. 
+
+We design scalable, real-time AI solutions using Generative AI, Large Language Models, and Agentic AI architectures. Our mission is to deliver helpful, personalised and engaging experiences like smart recommendations, listing insights and conversational support that empower users throughout their journey. These two squads work closely with many teams across the business, including several teams in Consumer Product & Experience, Customer Product & Experience, PropTrack and Data Services, among others.
+
+What the role is all about
+As a Senior Engineering Manager, you will play a pivotal role in driving technical excellence and delivery success across your squads. You will be responsible for delivering on the product roadmap by enhancing technical solutions, ensuring engineering quality, and leading a team of individual contributors to achieve organisational goals. This role combines technical leadership, people management, and delivery leadership to foster a collaborative, innovative, and high-performance environment. 
+
+As a Senior EM you operate at a larger scale in a highly complex environment with Machine Learning and Data Scientists. You will oversee multiple squads.  You are practiced at operating at scale by delegating critical technical decisions and team practices to staff and senior engineers in your team.  As a Senior EM you will identify and develop individuals into both expert and engineering management pathways.
+
+Contribute to and define the tech strategy for the business unit.
+Involvement in building the product strategy, vision & OKRs through representing the technical aspects.
+Oversee and execute the technical strategy of the team.
+Lead the technical design of solutions within your team.
+Maintain systems health and engineering compliance.
+Oversee the technical operation of systems your team owns.  At REA we practice “You Own it You Run”.  Ensure your team contributes to your groups support roster & supports resolution of related incidents.
+Contribute to product discovery through tech direction & feasibility assessments.
+Lead engineering excellence through best practices, including design patterns, clean code, Test-Driven Development (TDD), and unit testing.
+Lead adoption of REA standards & platforms and align with tech strategy in your team.
+Contribute the tech component of GIF business cases.
+Support other teams that use or contribute to your teams' systems & service, e.g. through federation.
+Review mission critical PRs.
+Review IPRs and pairing where appropriate.
+Guide and manage individual contributors to foster career development and team cohesion, typically you will have 5-12 engineers in your team as your direct reports.   
+Deliver the product roadmap, tracking and communicating progress.
+Provide forecasts and timelines contributing to product roadmaps.
+Who we’re looking for
+
+Extensive experience in leading and motivating multiple engineering teams in an agile environment
+Advanced technical expertise in the data science/machine learning field
+Proven ability to craft robust end to end solutions and manage intricate production systems with proficiency
+Demonstrated experience in building strong interpersonal relationships, ensuring seamless collaboration among diverse groups.
+Strong ability to navigate multiple priorities and foster continuous improvement
+You champion delivery success and innovation by leading with integrity, encouraging your team to adopt best practices and align with strategic objectives
+Proactively addressing challenges, you foster a culture of constant enhancement, driving both technical progress and team unity"""
+
+COMPANY_HISTORY="""Make an impact leading exciting squads utilising new AI technologies!
+Work across multiple leading property brands impacting millions of users!
+Permanent full time role based in Richmond
+We're REA
+With bold and ambitious goals, REA Group  is changing the way the world experiences property. No matter where you're at on your property journey, we're here to help with every step – whether that's finding or financing your next home.
+
+Our people are the key to our success. At the heart of everything we do, is a thriving culture centred around high performance and care. We are purpose driven and collaborative, which drives innovation and our ability to make a real impact. As such, we’re proud to have been named in Australia’s “Top 5” Best Workplaces two years in a row, as well as being recognised as a Best Workplace for Women.
+"""
+
+HIRING_MANAGER_EXPERIENCE="""
+<About>
+Engineering Manager at REA Group
+Lead author of Effective Machine Learning Teams 👉 https://www.oreilly.com/library/view/effective-machine-learning/9781098144623/
+</About>
+
+<Experience>
+
+ML Engineering Manager
+REA Group · Full-time
+Oct 2025 - Present · 5 months
+Melbourne, Victoria, Australia · 
+
+ML Engineering Manager
+Xero · Full-time 
+Sep 2024 - Oct 2025 · 1 yr 2 months
+Melbourne, Victoria, Australia 
+
+Senior ML Engineer
+Woolworths Group · Full-time
+May 2024 to Sep 2024 · 5 months
+Melbourne, Victoria, Australia
+
+Lead ML Engineer
+Thoughtworks Full-time
+Sep 2022 - May 2024 · 1 yr 9 months
+Melbourne, Victoria, Australia
+Skills: Machine Learning · Systems Thinking · Problem Solving · Google Cloud Platform (GCP) · Prompt Engineering · SQL · TensorFlow · Data Engineering · Scikit-Learn · Terraform · Data Products · Cloud Services · Natural Language Processing (NLP) · Communication · Coaching · DevOps · MLOps · Keras · Data Analytics · AWS SageMaker · CI · Large Language Models (LLM) · Continuous Integration and Continuous Delivery (CI/CD) · PyTorch · Project Management · Amazon Web Services (AWS) · Google BigQuery · Large-scale Data Analysis
+
+Lead Machine Learning Engineer
+Thoughtworks Full-time
+Jan 2021 - May 2024 · 3 yrs 5 months
+Melbourne, Victoria, Australia 
+Skills: Lean Engineering · Program Delivery Management · Machine Learning · Systems Thinking · ML Engineering · MLOps · Generative AI
+
+Software Developer
+Thoughtworks Full-time
+Dec 2016 - Sep 2022 · 5 yrs 10 months
+Singapore
+Skills: Unix · React.js · Software Development · JavaScript · Front-End Development
+
+
+Teaching Assistant
+General Assembly
+Aug 2016 - Dec 2016 · 5 months
+Singapore
+At General Assembly's Web Development Immersive course, I assist in training a class of 20 students to be full-stack developers. My competencies include the following languages (Javascript, Ruby, Python), frameworks (ExpressJS, AngularJS, Rails) and development workflows (agile development, Git, lean UX).
+
+Assistant Director
+Ministry of Trade and Industry
+Aug 2013 - May 2016 · 2 yrs 10 months
+Singapore
+Responsibilities: I have two years of corporate experience in (i) research and analysis, (ii) project management, (iii) trade negotiations and (iv) strategic planning.
+Project: In my research portfolio, I analysed trade flows and sectoral data to produce policy insights and actionable recommendations.
+I also saw through the development and deployment of a Customer Relationship Management (CRM) system for MTI. As the project manager on the business end, I managed the collaboration process with end-users and MTI management to ensure that the system met the needs of business users and provided a streamlined user experience. 
+I was part of the team which renegotiated the Singapore-Australia Free Trade Agreement and the Korea-Singapore Free Trade Agreement (KSFTA). I also provided support to MTI's senior management on MTI's organisational/strategic planning.
+I have two years of corporate experience in (i) research and analysis, (ii) project management, (iii) trade negotiations and (iv) strategic planning. In my research portfolio, I analysed trade flows and sectoral data to produce policy insights and actionable recommendations. I also saw through the development and deployment of a Customer Relationship Management (CRM) system for MTI. As the project manager on the business end, I managed the collaboration process with end-users and MTI management to ensure that the system met the needs of business users and provided a streamlined user experience. I was part of the team which renegotiated the Singapore-Australia Free Trade Agreement and the Korea-Singapore Free Trade Agreement (KSFTA). I also provided support to MTI's senior management on MTI's organisational/strategic planning.
+Skills: Hypothesis Testing · Quantitative Research
+</Experience>
+"""
